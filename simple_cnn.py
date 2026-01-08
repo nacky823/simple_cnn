332e9a1f45a9ad5ef7c5b93caac6117119d628ca
nacky823 <youjiyongmu4@gmail.com>
2026-01-08 12:26:13 +0900

    add relu

--- simple_cnn.py
+++ simple_cnn.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+
+def relu(Z: np.ndarray) -> np.ndarray:
+    return np.maximum(Z, 0.0)
+
+
+if __name__ == "__main__":
+    x = np.array([-1.0, 0.5, 2.0], dtype=np.float32)
+    print("relu:", relu(x))
