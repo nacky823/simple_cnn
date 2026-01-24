6852abec9e2f2eefeb096190b01c0908286c8bbf
nacky823 <youjiyongmu4@gmail.com>
2026-01-25 01:22:30 +0900

    Update miss grid

--- simple_cnn.py
+++ simple_cnn.py
@@ -396,3 +396,7 @@ if __name__ == "__main__":
         X_test_s, y_test_s, te_logits,
         path="outputs/pred_grid.png", max_images=25, ncols=5
     )
+    save_misclassified_grid(
+        X_test_s, y_test_s, te_logits,
+        path="outputs/misclassified.png", max_images=25, ncols=5
+    )
